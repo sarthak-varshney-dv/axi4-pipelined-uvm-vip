13ac5614113fb2328cff5ac59aa53334bb75da71
sarthak-varshney-dv <sarthak.varshney.dv@gmail.com>
2026-04-14 03:36:54 +0400

    drv item created

--- sv_axi4_pkg/sv_axi4_item_drive.sv
+++ sv_axi4_pkg/sv_axi4_item_drive.sv
@@ -4,15 +4,23 @@
 class sv_axi4_item_drv extends sv_axi4_item_base;
   
   
+rand sv_axi4_id         id;
+rand sv_axi4_dir        dir;
+rand sv_axi4_addr       addr;        // starting address
+rand logic [7:0]        burst_len;   // number of beats MINUS 1
+rand logic [2:0]        burst_size;  // bytes per beat (as power of 2)
+rand sv_axi4_burst      burst_type;  // FIXED, INCR, or WRAP
+rand sv_axi4_data       wdata[];     // dynamic array — one entry per beat
 
-rand sv_axi4_dir dir;
+  // Response fields (B / R)
+  sv_axi4_response                           bresp;       // write response
+  sv_axi4_response                           rresp[];     // read response per beat
+  bit  [`SV_AXI4_MAX_DATA_WIDTH:0]           rdata[];     // read data per beat
 
-rand sv_axi4_addr  addr;        // starting address
-rand logic [7:0]             burst_len;   // number of beats MINUS 1
-rand logic [2:0]             burst_size;  // bytes per beat (as power of 2)
-rand sv_axi4_burst                 burst_type;  // FIXED, INCR, or WRAP
-rand sv_axi4_data  wdata[];     // dynamic array — one entry per beat
-
+  //delays
+  rand int unsigned               pre_addr_delay;  // cycles before asserting AW/ARvalid
+  rand int unsigned               pre_data_delay;  // cycles before asserting Wvalid (write)
+  rand int unsigned               pre_resp_delay;  // slave: cycles before asserting Bvalid/Rvalid
 
 constraint c_data_array_size {
     wdata.size() == burst_len + 1;
--- sv_axi4_pkg/sv_axi4_types.sv
+++ sv_axi4_pkg/sv_axi4_types.sv
@@ -7,10 +7,14 @@ typedef bit[`SV_AXI4_MAX_DATA_WIDTH-1:0] sv_axi4_data;
 
 typedef bit[`SV_AXI4_MAX_ADDR_WIDTH-1:0] sv_axi4_addr;
 
+typedef bit[`SV_AXI4_MAX_ID_WIDTH-1:0] sv_axi4_id;
+
+
 typedef enum {SV_AXI4_READ=0 , SV_AXI4_WRITE=1} sv_axi4_dir;
 
 typedef enum {SV_AXI4_ERR=1 , SV_AXI4_OK=0}  sv_axi4_response;
 
- typedef enum logic [1:0] { FIXED = 2'b00, INCR  = 2'b01, WRAP  = 2'b10 } sv_axi4_burst;
+
+typedef enum bit [1:0] { FIXED = 2'b00, INCR  = 2'b01, WRAP  = 2'b10 } sv_axi4_burst;
 
 `endif
